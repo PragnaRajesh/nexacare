9249daf706feecfeeffca8642e24018c31971725
Pragna R <pragrajesh@gmail.com>
2025-02-23 21:20:14 +0530

    Integrated custom login page

---
+++ frontend/src/frontend_components/login.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/login.css"; // Import the CSS file
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Welcome to NexaCare</h2>
+        <form>
+          <input type="email" placeholder="Email" className="input-field" />
+          <input type="password" placeholder="Password" className="input-field" />
+          <button className="login-btn" type="submit">Login</button>
+        </form>
+        <p>Don't have an account? <a href="/signup">Sign up</a></p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
